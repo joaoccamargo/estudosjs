9155955c321d9f80218a9bb67da7b2d863da15c6
João Camargo <jclaudio00_12@hotmail.com>
2022-09-28 20:39:28 -0300

    Create 34-declarar-funcoes.js

---
+++ Basic/34-declarar-funcoes.js
@@ -0,0 +1,24 @@
+// Declaração de função (function hoisting)
+falaOi();
+
+function falaOi(){
+    console.log('Oi')
+}
+
+// First-class objects(Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+    console.log('Sou uma constante recebendo uma função')
+}
+
+souUmDado();
+
+function executaFuncao(funcao){
+    funcao();
+}
+executaFuncao(souUmDado);
+
+// Arrow function 
+const funcaoArrow = () => {console.log('Sou uma arrow function')}
+funcaoArrow();
+

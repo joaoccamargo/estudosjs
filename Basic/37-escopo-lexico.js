a72eb739bfb1f6e982ae19bedd4799fd2015b497
João Camargo <jclaudio00_12@hotmail.com>
2023-05-15 21:01:14 -0300

    escopo lexico

---
+++ Basic/37-escopo-lexico.js
@@ -0,0 +1,13 @@
+const nome = 'Jose'
+
+function falaNome(){
+    const nome = 'Jose Silva'
+    console.log(nome)
+}
+
+function usaFalanome(){
+    const nome = 'Rita'
+    console.log(nome)
+}
+falaNome()
+usaFalanome()

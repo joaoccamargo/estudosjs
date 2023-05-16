b902d4b8dc7c2c7b0015f639d13e77fa5de804a3
João Camargo <jclaudio00_12@hotmail.com>
2023-05-15 21:31:19 -0300

    factory function

---
+++ Basic/41-funcoes-fabricas.js
@@ -0,0 +1,23 @@
+// Factory Functions
+function criaPessoa(nome, sobrenome, altura, peso){
+    return {
+        nome, 
+        sobrenome,
+        fala(assunto){
+            return `${this.nome} está ${assunto}`
+        },
+        altura,
+        peso,
+        imc(){
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2);
+        }
+    }
+}
+
+const p1 = criaPessoa('Luiz', 'Silva', 1.8, 80)
+const p2 = criaPessoa('Maria', 'Joaquina', 1.60, 45)
+console.log(p1.fala('falando sobre JS'))
+console.log(p1.imc())
+console.log(p2.fala('falando sobre JS'))
+console.log(p2.imc())

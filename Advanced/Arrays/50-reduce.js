b68d660cb56dfbe0f58dc4847da5e038f5261f30
João Camargo <jclaudio00_12@hotmail.com>
2023-12-06 20:47:44 -0300

    reduce js

---
+++ Advanced/Arrays/50-reduce.js
@@ -0,0 +1,13 @@
+// Some todos os números (Reduce)
+// Retorne um array com os pares (Filter)
+// Retorne um array com o dobro dos valores (Map)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const total = numeros.reduce((acumulador, valor, indice, array) => {
+    acumulador += valor;
+    //console.log(acumulador, valor);
+    return acumulador;
+}, 0)
+
+console.log(total);

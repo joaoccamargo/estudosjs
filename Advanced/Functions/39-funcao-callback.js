654d9eb6c0a095952d7560e1f125b72b240f1085
João Camargo <jclaudio00_12@hotmail.com>
2023-12-05 21:11:49 -0300

    funcao callback

--- Advanced/Functions/39-funcao-callback.js
+++ Advanced/Functions/39-funcao-callback.js
@@ -1,7 +1,37 @@
-function funcaoFunc1(){
-   setTimeout(function(){
-    console.log('func1')
-   }, 500) 
+// Função CallBack
+function rand(min = 1000, max = 3000){
+   const num = Math.random() * (max-min) + min;
+   return Math.floor(num);
 }
-funcaoFunc1()
-console.log('Ola')
+
+function funcaoUm(callback){
+   setTimeout(function(){
+      console.log('F1');
+      if(callback) callback();
+   }, rand());
+}
+
+function funcaoDois(callback){
+   setTimeout(function(){
+      console.log('F2');
+      if(callback) callback();
+   }, rand());
+}
+function funcaoTres(callback){
+   setTimeout(function(){
+      console.log('F3')
+      if(callback) callback();
+   }, rand());
+}
+
+function f1Callback(){
+   funcaoDois(f2Callback)
+}
+
+function f2Callback(){
+   funcaoTres(f3Callback);
+}
+
+function f3Callback(){
+   console.log('Olá callback');
+}
